f00adb72b6fd2914a033465190194ad46f65f325
eileenmcnamara <eileen.aylin.mcnamara@gmail.com>
2025-07-31 15:34:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -61,12 +61,13 @@ toggleButton?.addEventListener('click', () => {
 const descriptions = {
   "Page1.jpg": "Pop-art inspired digital sketch created in Adobe Illustrator.First experiment with typography and layering techniques, exploring text as visual expression.",
   "Page3.jpg": "Shape-based digital fashion sketch using Adobe Illustrator. Ready mannequin styled with geometric forms and bold contrast for a futuristic look.",
-  "Page5.jpg": "Geometric logo design created in Adobe Illustrator. First attempt at digital lettering — exploring symmetry and graphic rhythm.",
-  "Page7.jpg": "Animated frame study in Krita",
-  "Page9.jpg": "Acrylic still life on canvas",
-  "Page11.jpg": "Pencil sketch from reference photo",
-  "Page13.jpg": "Animated frame study in Krita",
-  "Page15.jpg": "Acrylic still life on canvas",
-   "Page17.jpg": "Abstract gradient study created in Adobe Illustrator. First try with shapes, shadows, and 3D raster mesh effects to explore depth and light using bold color transitions."
+  "Page5.jpg": "Geometric logo design for Besiktas Football Team created in Adobe Illustrator. First attempt at digital lettering — exploring symmetry and graphic rhythm.",
+  "Page9.jpg": "Adobe Illustrator study using colorful star shapes. Focus on symmetry, layering, and bold outlines.",
+  "Page11.jpg": "Collage made in Adobe Photoshop, inspired by Drake and PARTYNEXTDOOR promo visuals. Combines concert tickets, merch, and digital media to recreate an OVO-style PR look.",
+  "Page13.jpg": "Mixed media collage using Adobe Photoshop and Illustrator. Combines bold star shapes with music visuals from Drake and PARTYNEXTDOOR to create a dynamic, fan-inspired layout.",
+  "Page15.jpg": "3D perspective study in Adobe Illustrator. Created using geometric shapes, gradient lines, and star motifs to explore depth, motion, and bold contrast.",
+  "Page17.jpg": "Abstract gradient study created in Adobe Illustrator. First try with shapes, shadows, and 3D raster mesh effects to explore depth and light using bold color transitions."
+  "Page19.jpg": "Colorful star composition made in Adobe Illustrator. Practicing shape building, layering, and vibrant color contrast."
+  "Page21.jpg": "Adobe Illustrator illustration of cereal and spoon. Practicing shape repetition, outlines, and playful food design."
 };
 
